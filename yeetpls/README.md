4690b0ebbe860ccf919875b7148bf7d704f2bbe7
RivenSkaye <pretender0906@gmail.com>
2021-01-22 23:39:47 +0100

    Initialization successful, dynamic loading achieved

--- yeetpls/README.md
+++ yeetpls/README.md
@@ -7,14 +7,28 @@ If this doesn't work, it notifies the user with an `error` message and exits.
 This script does not care about where it's being run from. So long as mpv can resolve the file names from the playlist, it's assumed this script can as well with no extra info
 
 ## Parsers ##
-A new parser for a type of playlist files should provide a couple of functions with standardized names:
-- `read_to_playlist`: Should take in a string (the full contents of the playlist file) and return a list of just the file entries. These should be kept as they are.
-	- If supplied files are just filenames, return just filenames;
-	- If supplied files are a full path, return full paths.
-	- If supplied files are Windows paths, return Windows paths
-	- And if they're relative, return them relative.
-- `format_playlist`: Should take in a list of items and transform it to a playlist of the type it parses. It should return this as one long string
-	- Stick to whatever the spec for the playlist type is
-	- Ensure it's valid enough that it can be passed to mpv's `--playlist` switch and just play everything
-Whatever else these parsers do internally is irrelevant. Make them perform black magic for all I care,
-so long as it translates between mpv's internal playlist objects and the type of playlist it processes.
+A new parser for a type of playlist files should provide at least one function:
+- `format_pls`:
+	- Arguments given to a parser are always the same, in the given order:
+		- The playlist as read from the file, as a string.
+			- This is the _original file_ and entries have **not yet been removed**.
+		- The `playlist` object as returned by mpv's `mp.get_property_native("playlist")`.
+			- In Lua, this becomes an indexed table of tables:
+			- Top level: `ipair`s of a number and a table to determine the order;
+			- Inner table: `pair`s of Stream Type (string) and the actual URL.
+		- The total amount of entries in the playlist.
+		- The current position in the playlist
+	- The value returned should be a string that can be written to a playlist file, according to the playlist spec.
+	- **Make sure to add it to the module's exported function list**. The parser will be `require`d dynamically so if you don't expose `parser.format_pls`, it can't be used.
+Whatever else these parsers do internally is irrelevant, make them perform black magic for all I care.
+So long as it translates between mpv's internal playlist objects and the type of playlist it processes, this code is gonna be happy with it.
+
+## Notes ##
+This script assumes that **there is no shuffle applied**. It was made with the intention of automating the entire process from acquiring anime down to watching the show without doing anything
+other than pointing mpv to a playlist. I personally set up a simple script to run on download completion that automatically generates a file `playlist.txt` which is just a list of file names
+in the directory that don't match certain patterns.
+
+If you apply shuffle, make sure that the playlist format you use gets processed by a parser that actually filters the entries by comparison. The `txt` parser is guaranteed to provide
+this functionality, because the file format is extremely easy to use and the largest workload is extracting all remaining file names.
+
+~~the `txt` parser is also the only one guaranteed to actually exist. I'm lazy and if it works for _me_, I'm usually happy~~
--- yeetpls/main.lua
+++ yeetpls/main.lua
@@ -2,10 +2,12 @@
 local opts = require("mp.options")
 local utils = require("mp.utils")
 local msg = require("mp.msg")
+local filetype = nil -- used internally in places for the error state
+local mpv_pls = nil -- populated internally with mpv's playlist, so a list of
 local parser = nil
+local parser_name = nil -- This value is only used when stuff errors
+local pls_old = nil
 local options = {
-	-- The OS type, or an approximation anyway. auto makes the script check it
-	system = "auto",
 	-- whether or not to auto-delete playlist entries after playing them
 	auto_delete_entries = true, -- (*true | false)
 	-- Whether to delete the playlist file if the last file is played
@@ -14,35 +16,67 @@ local options = {
 	info_on_start = true, -- (*true | false)
 	-- Print info like remaining titles and title on the next entry
 	info_on_next = false, -- (true | *false)
+
 	-- The type of playlist, used for processing defaults internally. Default is "auto" and makes the script check it
 	-- This list of filetypes is arbitrary and "auto" will allow for parsing anything that has a parser script available
 	playlist_type = "auto", -- ("txt" | "m3u" | "m3u8" | "pls" | *"auto")
 	-- The playlist file. Due to limitations in the Lua env, this needs to be passed as a script-opt.
 	-- This playlist is parsed internally and only parses playlist types for which a parser is available.
-	playlist = "None" -- ("None" | "[/path/to/]<playlist_file.ext>")
+	playlist = "None", -- ("None" | "[/path/to/]<playlist_file.ext>")
+	-- Whether to create the playlist file if it doesn't exist yet
+	create_file = false -- (true | *false)
 }
 opts.read_options(options, "yeetpls")
 
+-- Basic initialization. Wrapped in a function so we can properly exit the script if anything fails
 function base_init()
-	--check OS type, borrowed from https://github.com/jonniek/mpv-playlistmanager/blob/8f01f38d71bdf02a3b61988a940a5fc0db3b573c/playlistmanager.lua#L171
-	if options.system=="auto" then
-	  local o = {}
-	  if mp.get_property_native('options/vo-mmcss-profile', o) ~= o then
-	    options.system = "windows"
-	  else
-	    options.system = "linux"
-	  end
-	end
-	if options.playlist_type == "auto" then
-		local filetype = options.playlist:reverse():match('.*%p'):reverse():sub(2)
+	if options.playlist_type == "auto" then -- Determine the type of playlist, this is based on the extension
+		filetype = options.playlist:reverse():match('.*%p'):reverse():sub(2)
 		print("Reading playlist of type "..filetype)
-		parser = require(filetype.."-parser")
+		-- More than willing to accept PRs that add tables to map extensions to playlist types,
+		-- in order to allow one parser to be used with any legal file type extension
+	else
+		filetype = options.playlist_type -- It's expected that users know what they're doing when setting this opt
+		-- If you named your file "some.esoteric.format.kekw", but it's just plaintext, you'll wanna set this.
+		-- If it's anything that just lists a file per line like it's `ls`, leave this option alone.
+		-- By default, it tries to parse unknown filetypes as `txt`
 	end
+	-- Not sure what happens if I define a new variable and use an existing one,
+	-- better make sure both exist.
+	local hasparser = nil
+	-- Test if we have a parser for this
+	hasparser,parser = pcall(require, filetype.."-parser")
+	if not hasparser then
+		-- default to the txt-parser
+		parser = require("txt-parser")
+		parser_name = "txt-parser"
+	else
+		-- We have a parser, save the name in case of errors
+		parser_name = filetype.."-parser"
+	end
+	-- Open the file for appending and reading, this tests for both read and write permissions.
+	local pls_file,err,errcode = io.open(options.playlist, "a+")
+	if err then -- We don't have read and/or write, error and exit
+		print("Recieved error code "..errcode..": "..err..".\nCannot process playlist, exiting...")
+		return false
+	else
+		pls_old = pls_file:read("*all") -- Save the old playlist, should be fairly small
+		pls_file:close() -- Close the file properly
+		mpv_pls = mp.get_property_native("playlist") -- Array of objects => indexed table of tables. [{"filename", <file path>}...]
+	end
+	-- Everything was successful, time to actually do stuff here!
+	return true
 end
 
-if not options.playlist == "None" then
-	print("No playlist given in the form of --script-opts=yeetpls-playlist=playlist.ext\n\tExiting extension...")
+if options.playlist == "None" then
+	print("No playlist given to the --script-opts=yeetpls=<playlist file> switch. Exiting extension...")
 	return
 else
-	base_init()
+	if not base_init() then -- something caused a failure, exit.
+		print(parser_name.." was unable to parse this playlist type ("..filetype.."). Exiting...")
+		return
+	else
+		-- TESTING INFO, success!
+		print("loading "..parser_name.." was successful!")
+	end
 end
