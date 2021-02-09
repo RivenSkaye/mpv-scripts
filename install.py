5c5209b12461d7625afad027dcb31c7749a30c20
RivenSkaye <pretender0906@gmail.com>
2021-02-09 18:29:46 +0100

    Installer works for all scripts listed in the JSON

--- install.py
+++ install.py
@@ -5,7 +5,7 @@ in the 'master' branch of the git repository. It then proceeds to offer users
 the option to install or update any of the provided scripts in either the
 default user folders for mpv, or a folder of their own choosing.
 
-It fetches the information of installed scripts on every start of the script
+It fetches the information of available scripts on every start
 and only removes entries after installing. This means it may very well list
 scripts that are already installed and up-to-date.
 Changing this would require changing the behavior of the application and saving
@@ -18,82 +18,81 @@ import json
 import os
 import platform
 from pathlib import Path
+from typing import Dict
 
-print("Welcome to Riven's interactive installer!")
+print("Welcome to Riven's interactive installer!\n")
 
 # Guess what OS is causing trouble. Again... -_-'
 windows = True if platform.system().lower() == "windows" else False
-def_path;
+def_path = None;
+newline = None
+sep = None
+stop = False
+
 if windows:
-    print("I've determined you are on a Windows machine. If this is not the case, something is wrong and you'll have to download the scripts manually.")
-    def_path = "%APPDATA%\\mpv\\scripts\\"
+    print("I've determined you are on a Windows machine. If this is not the case,\n\tsomething is wrong and you'll have to download the scripts manually.\n")
+    def_path = f"{os.getenv('APPDATA')}\\mpv\\scripts\\"
+    newline = "\r\n"
+    sep = "\\"
 else:
     print("I've determined you're on a UNIX-like OS. Thanks for making a somewhat sane decision.")
-    def_path = "~.config/mpv/scripts/"
-print("Please give me a moment to fetch a list of available scripts. This process is faster on faster internet connections.")
+    def_path = f"{os.getenv('$HOME')}/.config/mpv/scripts/"
+    newline = "\n"
+    sep = "/"
+
+print("Please give me a moment to fetch a list of available scripts...\n")
 # Base URL so we only need to append file and folder names
 base_url = "https://raw.githubusercontent.com/RivenSkaye/mpv-scripts/master/"
-filelist = json.loads(req.urlopen(f"{base_url}scripts.json").read().decode(response.headers.get_content_charset()).replace("\r\n", "\n"))
+jsonreq = req.urlopen(f"{base_url}scripts.json")
+filelist = json.loads(jsonreq.read().decode(jsonreq.headers.get_content_charset()).replace("\r\n", "\n"))
 
-
-
-# Either pick default or the supplied folder. On user error, fuck the user.
-if "-default" in argv: # User chose default dirs
-    default = True
-elif argv[1] == "-scripts":
-    opts['scripts'] = argv[2] or 666
-    default = True if 666 else False
-    print("No path given or incorrect syntax used. Reverting to default.")
-else:
-    given = input('Please type the absolute or relative path to the MPV scripts directory, defaults to default paths.') or 'default'
-    if given.lower() == "default":
-        default = True
+def select_script(filelist):
+    if len(filelist.keys()) < 1:
+        return True
+    choices = ["[0] Intstall all scripts"]
+    keys = []
+    curnum = 1
+    for key in filelist:
+        choices.append(f"[{curnum}] Install {key}")
+        keys.append(key)
+        curnum = curnum + 1
+    choices.append("[-1] Stop selecting scripts and exit")
+    reply = int(input(f"Please select what script(s) to install: \n{newline.join(choices)}\n\n> "))
+    if reply > 0:
+        install_script(filelist[keys[reply-1]], keys[reply-1])
+        del filelist[keys[reply-1]]
+        return False
+    if reply < 0: # You know who you are that this is warranted :ogsmug:
+        return True
     else:
-        opts['scripts'] = given
+        for key in filelist:
+            install_script(filelist[key], key)
+        return True
 
-if not opts['scripts'].endswith("/"):
-    opts['scripts'] += "/"
-opts['scripts'] += "yeetpls/"
 
-if windows:
-    # expand %APPDATA%
-    opts['scripts'] = opts['scripts'].replace("%APPDATA%", os.getenv('APPDATA'))
-    # Replace forwardslashes with double backslashes, because >Windows
-    opts['scripts'] = opts['scripts'].replace("/", "\\")
-else:
-    # expand $HOME
-    opts['scripts'] = opts['scripts'].replace("~", os.getenv('$HOME'))
+def install_script(data: Dict, name: str):
+    url = data['base_url']
+    if len(data['optional']) > 0:
+        joiner = f"{newline} - "
+        opts = input(f"There are optional script files:{joiner}{joiner.join(data['optional'])}\nThese are recommended to have, would you like to install these? [Y/n]\n\n> ") or 'y'
+    targets = data['required'] + data['optional'] if opts.lower().startswith('y') else data['required']
+    out_path = input(f"Please provide the full path for your mpv scripts directory.\nJust hit enter if {def_path} is fine.\n\n> ") or def_path
+    if not out_path.endswith(sep):
+        out_path += sep
+    if data['multifile']:
+        out_path += f"{name}{sep}"
+    Path(out_path).mkdir(parents=True, exist_ok=True)
+    for t in targets:
+        scriptfile = f"{out_path}{t}"
+        scriptreq = req.urlopen(url+t)
+        content = scriptreq.read().decode(scriptreq.headers.get_content_charset()).replace("\r\n", "\n")
+        with open(scriptfile, "w+") as f:
+            f.writelines(content.split("\n")) # Make sure to have Python handle the system's newlines
+    return
 
-# Confirm that the user agrees to the path
-confirmation = input(f"Is the path '{opts['scripts']}' correct? (Yes, it must end in yeetpls) [Y/n]") or "y"
-if not confirmation.lower() == "y":
-    print("Please run the installer again with the correct path or the '-default' option.")
-    exit(1)
-
-# Create any missing directories
-Path(opts['scripts']).mkdir(parents=True, exist_ok=True)
-
-# Get the filelist off git
-response = req.urlopen(base_url.format("filelist.txt"))
-# Make sure we get one file per line, without trailing \r if the file happens to have them.
-filelist = response.read().decode(response.headers.get_content_charset()).replace("\r\n", "\n").split("\n")
-for line in filelist:
-    # Trailing newline be like "HAHA NOPE". Also allows for """comments""" in the file. No idea why I did that.
-    if len(line) < 1 or line.startswith('#'):
-        # skip empty lines properly. If they somehow end up in the middle, don't die
-        continule
-    # Fetch the fie
-    url = base_url.format(line)
-    request = req.urlopen(url)
-    # prepare it for directly writing to a file
-    content = request.read().decode(response.headers.get_content_charset()).replace("\r\n", "\n")
-    if windows: # Keep into account retard OS (Python should handle it, but I don't wanna risk anything here)
-        content.replace("\n", "\r\n") # at least we won't get \r\r\n this way
-    # Write the file with its proper name. Design of the script depends on this
-    with open(opts['scripts']+line, 'w+') as f:
-        f.write(content)
+while not select_script(filelist): pass
 
 # If we get here without errors, we're done. And it's good.
-print(f"Should've been successful installing all required files to '{opts['scripts']}'. Thanks for using me and don't forget to run me again for upates!")
+print(f"\nShould've been successful installing all requested scripts. Thanks for using me and don't forget to run me again for updates!")
 # Bedtime
 exit(0)
